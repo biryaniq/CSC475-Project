17c2e1109afd27c84aa18786f203434a49f72adc
biryaniq <49655956+biryaniq@users.noreply.github.com>
2024-03-17 11:48:06 -0700

    created component dir, file upload component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,19 @@ import Head from "next/head";
 import { analyzeFile } from "@/utils/apiUtils";
 import { useMutation } from "react-query";
 import { useState } from "react";
+import FileUploader from "@/components/FileUploader"
+
+const url = "/api/upload";
+
+interface ContainerProps {
+  children: React.ReactNode
+}
+
+const Container = ({ children }: ContainerProps) => (
+  <div className="flex flex-col items-center justify-between gap-4 min-h-60 bg-zinc-800 w-full max-w-2xl py-10 px-4 rounded-xl h-fit">
+    {children}
+  </div>
+)
 
 export default function Home() {
   const [chordsUsed, setChordsUsed] = useState<string[] | undefined>(undefined);
@@ -27,6 +40,47 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Container>
+        <h1 className="text-2xl font-bold">File Uploader</h1>
+        <FileUploader
+          url={url}
+          acceptedFileTypes={[
+            "image/png",
+            "image/jpeg",
+          ]}
+          maxFileSize={100}
+          label="Max File Size: 1MB"
+          labelAlt="Accepted File Types: png, jpeg"
+        />
+      </Container>
+      <Container>
+        <h1 className="text-2xl font-bold">File Uploader</h1>
+        <FileUploader
+          url={url}
+          acceptedFileTypes={[
+            "image/png",
+            "image/jpeg",
+          ]}
+          allowMultiple={true}
+          maxFileSize={100}
+          label="Max File Size: 100MB (multiple)"
+          labelAlt="Accepted File Types: png, jpeg"
+        />
+      </Container>
+      <Container>
+        <h1 className="text-2xl font-bold">File Uploader</h1>
+        <FileUploader
+          url={'https://example.com'}
+          acceptedFileTypes={[
+            "image/png",
+            "image/jpeg",
+          ]}
+          allowMultiple={true}
+          maxFileSize={100}
+          label="Max File Size: 100MB (non-existent endpoint)"
+          labelAlt="Accepted File Types: png, jpeg"
+        />
+      </Container>
       <div>
         <button onClick={() => analyzeFileMutation()}>upload music!</button>
         <div>
